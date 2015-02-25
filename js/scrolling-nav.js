66b7c4c9899d056e558e6a9c4edef27e136323ab
Jose Luiz Tafarel <jltafarel@gmail.com>
2015-02-25 20:49:39 -0300

    melhorando animação do menu

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -2,8 +2,10 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".logo").css("width","85px");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".logo").css("width","100px");
     }
 });
 
